7ba433393acbe8ab942ce3f73ddf7ccb61267c77
Pipmor <139385966+Pipmor@users.noreply.github.com>
2023-12-14 15:46:52 +0600

    Создал заготовку проекта

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+
+const App = () => {
+  return (
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={} />
+        </div>
+      </BrowserRouter>
+  );
+};
+
+export default App;
+
+
